9c6139c28f6d7628a9e83740da3a7e0d0c291407
woleEkanola <woleekanola@gmail.com>
2019-03-05 12:20:01 +0100

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyC0VMqM6VEp5ge-zzT-v6oahqMB_7Ulynk",
+    authDomain: "vuefire-83aa3.firebaseapp.com",
+    databaseURL: "https://vuefire-83aa3.firebaseio.com",
+    projectId: "vuefire-83aa3",
+    storageBucket: "vuefire-83aa3.appspot.com",
+    messagingSenderId: "945377903922"
+  }
